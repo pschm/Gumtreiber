1338f4468cfd3a3775e36f2a556d7eeef4c6880e
nschmit1 <Nathalie-Schmitz@hotmail.com>
2019-01-09 12:25:50 +0100

    menu stuff :(

--- app/src/main/java/de/psst/gumtreiber/LoginActivity.java
+++ app/src/main/java/de/psst/gumtreiber/LoginActivity.java
@@ -1,13 +1,13 @@
 package de.psst.gumtreiber;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
     }
 }
