d1ffb158c978cb2058939c32eacc789289bc9348
nschmit1 <Nathalie-Schmitz@hotmail.com>
2019-02-12 14:32:27 +0100

    Added MainViewModel

---
+++ app/src/main/java/de/psst/gumtreiber/viewmodels/MainViewModel.java
@@ -0,0 +1,56 @@
+package de.psst.gumtreiber.viewmodels;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainViewModel extends AndroidViewModel {
+
+    //Shared Preferences Key's
+    private static final String PREFERENCES_KEY = "de.psst.gumtreiber";
+    private static final String LOCATION_STATE_KEY = "location";
+
+    //Location State of the User
+    private Boolean locationState;
+
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        fetchLocationStatus();
+    }
+
+
+    private SharedPreferences getPreferences() {
+        return getApplication().getSharedPreferences(PREFERENCES_KEY, Context.MODE_PRIVATE);
+    }
+
+
+    private void fetchLocationStatus() {
+        locationState = getPreferences().getBoolean(LOCATION_STATE_KEY, true);
+    }
+
+    /**
+     * Returns the LocationState
+     *
+     * @return LocationState
+     */
+    public Boolean getLocationState() {
+        return locationState;
+    }
+
+    /**
+     * Setting new location state
+     *
+     * @param newLocationState the new LocationState
+     */
+    public void setLocationState(Boolean newLocationState) {
+        getPreferences().edit().putBoolean(LOCATION_STATE_KEY, newLocationState).apply();
+        fetchLocationStatus();
+        Log.v("LOCATIONSTAET VIEWMODEL", locationState.toString());
+    }
+
+}
