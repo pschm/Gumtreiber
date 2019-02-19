99ad4f85856b6e4932db1eeb983ed7f7ae3144b5
RedAvenge <thepowerredstone@gmail.com>
2019-02-19 12:59:11 +0100

    Added recycler bin viewer to find friend layout;

--- app/src/main/java/de/psst/gumtreiber/ui/fragments/FindFriendsFragment.java
+++ app/src/main/java/de/psst/gumtreiber/ui/fragments/FindFriendsFragment.java
@@ -1,5 +1,141 @@
 package de.psst.gumtreiber.ui.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import de.psst.gumtreiber.R;
+import de.psst.gumtreiber.data.User;
+import de.psst.gumtreiber.ui.MainActivity;
+
+public class FindFriendsFragment extends Fragment {
+
+    private MainActivity activity;
+
+    //RecyclerView
+    private RecyclerView recyclerView;
+    private FindFriendsFragment.RecyclerAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        activity = (MainActivity) getActivity();
+        return inflater.inflate(R.layout.fragment_find_friends, container, false);
+    }
+
+    @Override
+    public void onDestroyView() {
+        activity.getSupportActionBar().setTitle(R.string.app_name);
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        activity.getSupportActionBar().setTitle("Freunde hinzufügen");
+
+        recyclerView = getActivity().findViewById(R.id.friends_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(activity);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        ArrayList<User> userList = new ArrayList<>();
+        userList.add(new User("ABC", "Der fast kopflose Kohls")); //TODO init korrekt machen
+        userList.add(new User("ABC", "Yggi der Elf"));
+        userList.add(new User("ABC", "Prof. Flitvikor"));
+        userList.add(new User("ABC", "Die bittere Birgit"));
+
+        adapter = new RecyclerAdapter(this, userList);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+
+
+    /**
+     * Recycle-bin-Adapter
+     */
+    private class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+        private FindFriendsFragment fragment;
+        private ArrayList<User> dataset;
+
+        private RecyclerAdapter(FindFriendsFragment fragment, ArrayList<User> dataset) {
+            this.fragment = fragment;
+            this.dataset = dataset;
+        }
+
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem_addfriend, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            final User user = dataset.get(position);
+
+            holder.txtUserName.setText(user.name);
+
+            holder.btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //TODO Freund hinzufügen
+                }
+
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataset.size();
+        }
+
+        public void refresh() {
+            notifyDataSetChanged();
+        }
+
+
+        private class ViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView txtUserName;
+            private Button btnAdd;
+
+            private ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                txtUserName = itemView.findViewById(R.id.txtUserName);
+                btnAdd = itemView.findViewById(R.id.btnAddFriend);
+            }
+        }
+
+    }
+}
+
+
+
+
+
+/* //TODO alten code löschen
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -59,7 +195,7 @@ public class FindFriendsFragment extends Fragment {
     /**
      * Adds a new small new LinearLayout as View to the main LinearLayout
      */
-    public void addToLinearLayout(final String b) {
+ /*   public void addToLinearLayout(final String b) {
 
         //TODO Leute richtig anzeigen ++ Schön machen
         LinearLayout smallLayout = new LinearLayout(getActivity());
@@ -93,7 +229,7 @@ public class FindFriendsFragment extends Fragment {
     /**
      * Refreshes the LinearLayout
      */
-    public void refreshLinearLayout() {
+ /*   public void refreshLinearLayout() {
 
         llPeopleList.removeAllViews();
         filterUserList();
@@ -107,7 +243,7 @@ public class FindFriendsFragment extends Fragment {
     /**
      * Filters People who are already on the FrindList out of the UserList
      */
-    public void filterUserList() {
+/*    public void filterUserList() {
 
         getFriendsList();
         for (String s : friendlist) {
@@ -120,8 +256,9 @@ public class FindFriendsFragment extends Fragment {
     /**
      * Gets the FriendList from SharedPreferences
      */
-    public void getFriendsList() {
+ /*   public void getFriendsList() {
         friendlist = getActivity().getApplicationContext().getSharedPreferences("FriendList", Context.MODE_PRIVATE).getStringSet("friendList", def);
     }
 
 }
+*/
