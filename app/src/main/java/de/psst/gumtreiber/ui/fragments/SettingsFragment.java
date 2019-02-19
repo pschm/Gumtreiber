ca1398f3b6f73c05793f0ad4c60101bdfa73d137
RedAvenge <thepowerredstone@gmail.com>
2019-02-19 16:11:52 +0100

    Added settings layout;

--- app/src/main/java/de/psst/gumtreiber/ui/fragments/SettingsFragment.java
+++ app/src/main/java/de/psst/gumtreiber/ui/fragments/SettingsFragment.java
@@ -5,19 +5,94 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 import de.psst.gumtreiber.R;
+import de.psst.gumtreiber.ui.MainActivity;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    private View fragmentView;
+    private MainActivity activity;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        fragmentView = inflater.inflate(R.layout.fragment_settings, container, false);
-        return fragmentView;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        activity = (MainActivity) getActivity();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        activity.resetActionBarTitle();
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        activity.setActionBarTitle(getString(R.string.title_settings));
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        initSummaries(user);
+    }
+
+    private void initSummaries(FirebaseUser user) {
+
+        if(user == null) findPreference("email").setSummary("INVALID");
+        else findPreference("email").setSummary(user.getEmail());
+
+        if(user == null) findPreference("nickname").setSummary("INVALID");
+        else findPreference("nickname").setSummary(user.getDisplayName());
+
+        if(user == null) findPreference("course").setSummary("INVALID");
+        else findPreference("course").setSummary("TODO"); //TODO
+
+
+        findPreference("version").setSummary(getString(R.string.app_version));
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference pref) {
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager == null) return false;
+
+        switch(pref.getKey()) {
+            //Account
+            case "email":
+
+                break;
+            case "password":
+
+                break;
+            case "nickname":
+
+                break;
+            case "course":
+
+                break;
+
+            //Filter
+            //...
+
+            //Info
+            case "version":
+
+                break;
+            default:
+                break;
+        }
+
+        return super.onPreferenceTreeClick(pref);
     }
 }
