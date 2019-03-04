69ec8568bada8c4fef4fa86c9ace77e266b6c3db
pschm <philipp.schmeier@gmail.com>
2019-03-04 14:43:16 +0100

    Added a function to validate user names

--- app/src/main/java/de/psst/gumtreiber/viewmodels/SettingsViewModel.java
+++ app/src/main/java/de/psst/gumtreiber/viewmodels/SettingsViewModel.java
@@ -1,6 +1,7 @@
 package de.psst.gumtreiber.viewmodels;
 
 import android.app.Application;
+import android.content.res.Resources;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -9,11 +10,14 @@ import java.util.Objects;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import de.psst.gumtreiber.R;
 import de.psst.gumtreiber.data.Course;
 import de.psst.gumtreiber.data.Firebase;
 import de.psst.gumtreiber.data.UserDataSync;
 
 public class SettingsViewModel extends AndroidViewModel {
+    private static final int MAX_USERNAME_LENGTH = 12;
+    private static final int MIN_USERNAME_LENGTH = 3;
 
     private String uid;
     private String token;
@@ -48,6 +52,25 @@ public class SettingsViewModel extends AndroidViewModel {
         return Firebase.getUser(uid, token).getName();
     }
 
+    /**
+     * Checks if a username meets the requirements
+     *
+     * @param name which will be reviewed
+     * @return "OK" if the username is valid or a corresponding error msg
+     */
+    public String validateUserName(@NonNull String name) {
+        Resources res = getApplication().getResources();
 
+        if (name.length() > MAX_USERNAME_LENGTH)
+            return res.getString(R.string.username_error_length_max);
+        if (name.length() < MIN_USERNAME_LENGTH)
+            return res.getString(R.string.username_error_length_min);
+        if (!name.matches("^[a-züäößA-ZÜÄÖ].*"))
+            return res.getString(R.string.username_error_starts_with);
+        if (!name.matches("[a-züäößA-ZÜÄÖ_-]*"))
+            return res.getString(R.string.username_error_allowed_characters);
+
+        return "OK";
+    }
 
 }
